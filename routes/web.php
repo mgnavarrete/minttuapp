8d589bbc837cd881c78293e8f8ffd7bd784b376b
mgnavarrete <mgnavarrete@uc.cl>
2024-12-17 22:13:41 -0300

    page

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\IndexController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Aquí es donde puedes registrar las rutas web para tu aplicación. Estas
+| rutas son cargadas por el RouteServiceProvider y todas ellas serán
+| asignadas al grupo de middleware "web". ¡Haz algo grandioso!
+|
+*/
+
+// LANDING PAGE //
+Route::get('/', [IndexController::class, 'index'])->name('index');  // Muestra la vista de inicio
