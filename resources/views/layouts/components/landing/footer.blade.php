03c8a8e7915a9f278eb9f4610b09ad01ba18f56a
mgnavarrete <mgnavarrete@uc.cl>
2024-12-19 13:33:55 -0300

    landing

--- resources/views/layouts/components/landing/footer.blade.php
+++ resources/views/layouts/components/landing/footer.blade.php
@@ -1,11 +1,5 @@
 
-            <div class="text-center landing-main-footer py-3">
-                <span class="text-muted fs-15"> Copyright © <span id="year"></span> <a
-                    href="javascript:void(0);" class="text-primary fw-semibold"><u>ynex</u></a>.
-                    Designed with <span class="fa fa-heart text-danger"></span> by <a href="javascript:void(0);" class="text-primary fw-semibold"><u>
-                    Spruko</u>
-                    </a> All
-                    rights
-                    reserved
-                </span>
-            </div>
+<div class="text-center landing-main-footer py-3">
+    <span class="text-muted fs-15"> Made with ❤️ by Menta
+    </span>
+</div>
